6084b89e110ffc79a3c757800e2d87e8d6903b71
unknown <kaihoshijo003@gmail.com>
2021-08-18 21:21:44 -0600

    The function search_related_images_for_name will take in the link provided by reverse_image_search and get an n, default is 5, amount of webpages related to that image. This is achieved through selenium, so the geckdriver is provided for the driver. As for a test image, an image of a Fulvous Whistling-Duck is used.

--- get_bird_name.py
+++ get_bird_name.py
@@ -1,10 +1,12 @@
 # importing the necessary modules
 import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+import os
 
 # setting up constants for the program
 LINK = "https://www.google.com/searchbyimage/upload"
-FILE_LOCATION = r"Image location"
+FILE_LOCATION = os.getcwd() + r"/bird_images/fuwd-2.jpg"
+GECKO_PATH = os.getcwd() + r"/geckodriver/geckodriver.exe"
 
 def reverse_image_search(img_location):
     """
@@ -25,3 +27,46 @@ def reverse_image_search(img_location):
     return response.headers['Location']
 
 search_result = reverse_image_search(FILE_LOCATION)
+print(search_result)
+
+def search_related_images_for_name(search_result, amount_of_pages=5):
+    """
+        Utilizes the previous reverse image search to find similar images
+        for the name of the bird provided in the image.
+
+            Parameters:
+                search_result (string): This is the string of the link to
+                google's reverse image search.
+                amount_of_pages (int): This is the amount of webpages returned
+                back to the user.
+            
+            Returns:
+                data_page (string list): A list of the top 5 websites related
+                to the reverse image search.
+    """
+    # starting the driver for selenium
+    driver = webdriver.Firefox(executable_path=GECKO_PATH)
+    # connecting the driver to the search result
+    driver.get(search_result)
+    
+    # getting the iur div
+    iur_div = driver.find_element_by_id("iur")
+    # getting the images universal data-attrid tag because this is all the 
+    # similar images to the one provided in the google search
+    images_universal_xpath = "//div[@data-attrid='images universal']"
+    images_universal_elements = iur_div.find_elements_by_xpath(images_universal_xpath)
+    print(len(images_universal_elements))
+
+    # getting the data-lpage attribute for each iur element
+    # appending that attribute to a data_page
+    data_page = []
+    for element in images_universal_elements[:amount_of_pages]:
+        data_page.append(element.get_attribute("data-lpage"))
+
+    # closing the firefox driver
+    driver.close()
+
+    # returning the data_page
+    return data_page
+
+search_related_images_for_name(search_result)
